51059e278952c98b81e6bdb034ca1ee91cf45077
Alex Gustafsson <alex-gustafsson@live.se>
2020-09-05 21:08:34 +0200

    Add character frequency map creation

--- README.md
+++ README.md
@@ -23,6 +23,7 @@ As of now, this repository contains data for the Swedish and English language. N
 #### Swedish
 
 1. Word frequencies - roughly 300 000 words and how often they occur.
+1. Character frequencies - roughly 600 characters and how often they occur.
 2. bigrams - roughly 1 600 000 unique bigrams.
 3. trigrams - roughly 2 150 650 unique trigrams.
 
--- scripts/processing/count.py
+++ scripts/processing/count.py
@@ -24,15 +24,22 @@ def count(output_directory: str, language: str) -> None:
 
     logger.info("Counting %d words", len(words))
 
-    count = {}
+    word_count = {}
+    character_count = {}
     for word in words:
-        if word not in count:
-            count[word] = 0
-        count[word] += 1
-    sorted_count = {k: v for k, v in sorted(count.items(), key=lambda item: item[1], reverse=True)}
+        for character in word:
+            if character not in character_count:
+                character_count[character] = 0
+            character_count[character] += 1
+        if word not in word_count:
+            word_count[word] = 0
+        word_count[word] += 1
+    sorted_word_count = {k: v for k, v in sorted(word_count.items(), key=lambda item: item[1], reverse=True)}
+    sorted_character_count = {k: v for k, v in sorted(character_count.items(), key=lambda item: item[1], reverse=True)}
 
     logger.info("Completed all jobs, storing compilation")
-    store(output_directory, "./compiled/{}/frequencies.json".format(language), json.dumps(sorted_count, indent=2))
+    store(output_directory, "./compiled/{}/word-frequencies.json".format(language), json.dumps(sorted_word_count, indent=2))
+    store(output_directory, "./compiled/{}/character-frequencies.json".format(language), json.dumps(sorted_character_count, indent=2))
 
 
 def main() -> None:
@@ -40,7 +47,7 @@ def main() -> None:
     global executor
 
     # Create an argument parser for parsing CLI arguments
-    parser = ArgumentParser(description="A threaded tool to count the frequency of words in large collections of textual content")
+    parser = ArgumentParser(description="A tool to count the frequency of words and characters in large collections of textual content")
     parser.add_argument("-l", "--language", required=True, type=str, help="The two letter language code for the language to clean")
     parser.add_argument("-c", "--cache", type=str, default="./frequency-data", required=False, help="The cache directory to use")
 
